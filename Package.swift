5e73595108802ec4c232d3bf5f94272134d820c0
Tomas Harkema <tomas@harkema.io>
2023-08-15 22:13:28 +0200

    downgrade

--- Package.swift
+++ Package.swift
@@ -5,10 +5,14 @@ let package = Package(
     name: "GEOSwift",
     platforms: [.iOS(.v9), .macOS("10.9"), .tvOS(.v9), .watchOS(.v2)],
     products: [
-        .library(name: "GEOSwift", targets: ["GEOSwift"])
+        .library(
+            name: "GEOSwift",
+            type: .static,
+            targets: ["GEOSwift"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/tomasharkema/geos.git", branch: "main")
+        .package(url: "https://github.com/tomasharkema/geos.git", revision: "9fb3f39fc7e3bf0d40c21e5fa740ac8a6a0e2315")
     ],
     targets: [
         .target(
